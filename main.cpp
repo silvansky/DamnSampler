7c8697b4c579a8aa44d524f45f25ed8e2440ed94
Valentine Silvansky <silvansky@users.noreply.github.com>
2010-08-31 10:57:13 +0000

    Initial release. Source code tested on Windows XP with MSVC 2008 compiler. Put FMOD library api in FMOD folder. Remark: shouldn't work with MinGW.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QApplication>
+#include "samplerwindow.h"
+#include <QCleanlooksStyle>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	a.setStyle(new QCleanlooksStyle);
+	a.setApplicationName("DamnSmpler");
+	a.setOrganizationName("silvansky");
+	SamplerWindow w;
+	w.show();
+	return a.exec();
+}
